f2677c2e326d0d0b43cd53353e112685d36ec725
isabelveronica <isabelvfr@gmail.com>
2022-12-16 15:55:42 -0300

    Resolução da Atividade

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 /**
  * Aplica a cor #BECCC3 no background do body da página
  */
-function background(){
-
-    /**
-     * Tarefa/Issue  1
-     *
-     * Desenvolva uma função capaz de trocar a cor do fundo da página, ou seja,
-     * alterar o background do body para a cor #BECCC3.
-     */
-    document.body.style.backgroundColor = #BECCC3;
+function background() {
+  /**
+   * Tarefa/Issue  1
+   *
+   * Desenvolva uma função capaz de trocar a cor do fundo da página, ou seja,
+   * alterar o background do body para a cor #BECCC3.
+   */
+  document.body.style.backgroundColor = #BECCC3;
 }
 
 /**
@@ -17,54 +16,62 @@ function background(){
  * e apresente o nome e o sobrenome de uma pessoa (separado por um espaço)
  * na div id='result'
  */
-function show(){
-
-     /**
-      * Issue  2
-      *
-      * Recupera os valores dos campos de texto cujo o id='fname' e id='lname' e apresente o nome
-      * e o sobrenome de uma pessoa (separado por um espaço) na div id='result'
-      */
-     let fname = document.getElementById('fname').value;
-     let lname = document.getElementById('lname').value;
-     result = document.getElementById('result').innerHTML = (fname + " " + lname);
+function show() {
+  /**
+   * Issue  2
+   *
+   * Recupera os valores dos campos de texto cujo o id='fname' e id='lname' e apresente o nome
+   * e o sobrenome de uma pessoa (separado por um espaço) na div id='result'
+   */
+  let fname = document.getElementById('fname').value;
+  let lname = document.getElementById('lname').value;
+  result = document.getElementById('result').innerHTML = fname + ' ' + lname;
 }
 
 /**
  * Filtra o array de objetos (data) de acordo com os caracteres
  * digitados no campo de texto id='name'
  */
-function search(){
+function search() {
+  var data = [
+    { name: 'Rodrigo' },
+    { name: 'Ricardo' },
+    { name: 'Fabio' },
+    { name: 'Alex' },
+    { name: 'Sílvia' },
+  ];
 
-    var data = [{name: "Rodrigo"}, {name: "Ricardo"}, {name: "Fabio"}, {name: "Alex"}, {name: "Sílvia"}];
+  /**
+   * Issue 3
+   *
+   * Utilizando o array (data) de objetos JSON acima, desenvolva um código que permita com que os
+   * usuários procurem pelo nome de pessoas. Assim, quando o usuário digitar a letra 'r' no campo
+   * de texto (id='name'), todas as pessoas que iniciarem com essa letra devem ser apresentados.
+   * Consequentemente, quando for digitado 'ro' o sistema deve filtrar e apresentar na div com o
+   * id='search' as pessoas cujo o nome inicia com 'ro'.
+   *
+   * Segue algumas funções que podem auxiliar na resolução da questão:
+   *      substr - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/substr
+   *      length - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/length
+   *      toUpperCase - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+   *
+   * Além disso, a função removeAllChildren abaixo também pode ser útil para o desenvolvimento da solução
+   */
+  let firstName = document.getElementById('name').value;
+  let filteredData = data.filter(
+    i =>
+      firstName.toUppercase() ===
+      i.name.substring(0, firstName.length).toUpperCase()
+  );
+  let search = document.getElementById('search');
 
-    /**
-     * Issue 3
-     *
-     * Utilizando o array (data) de objetos JSON acima, desenvolva um código que permita com que os
-     * usuários procurem pelo nome de pessoas. Assim, quando o usuário digitar a letra 'r' no campo
-     * de texto (id='name'), todas as pessoas que iniciarem com essa letra devem ser apresentados.
-     * Consequentemente, quando for digitado 'ro' o sistema deve filtrar e apresentar na div com o
-     * id='search' as pessoas cujo o nome inicia com 'ro'.
-     *
-     * Segue algumas funções que podem auxiliar na resolução da questão:
-     *      substr - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/substr
-     *      length - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/length
-     *      toUpperCase - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
-     *
-     * Além disso, a função removeAllChildren abaixo também pode ser útil para o desenvolvimento da solução
-     */
-    let firstName = document.getElementById('name').value;
-    let filteredData = data.filter (i => firstName.toUppercase() === i.name.substring(0,firstName.length).toUpperCase());
-    let search = document.getElementById("search");
+  removeAllChildren(search);
 
-    removeAllChildren(search);
-
-    filteredData.forEach(e) => {
-        let newDiv = document.createElement('div');
-        newDiv.innerText = e.firstName
-        search.appendChild(newDiv);
-    });
+  filteredData.forEach(e => {
+    let newDiv = document.createElement('div');
+    newDiv.innerText = e.firstName;
+    search.appendChild(newDiv);
+  });
 }
 
 /**
@@ -72,8 +79,8 @@ function search(){
  *
  * @param {*} node Um objeto HTML Node
  */
-function removeAllChildren(node){
-    while (node.hasChildNodes()) {
-        node.removeChild(node.firstChild);
-    }
+function removeAllChildren(node) {
+  while (node.hasChildNodes()) {
+    node.removeChild(node.firstChild);
+  }
 }
